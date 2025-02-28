3a053e7f5c89e2d693ae37ebc23c0549a5f5c665
j4n9k4 <janukananayakkara@gmail.com>
2025-02-28 16:31:07 +0530

    Logging the onStart function

--- app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -89,6 +89,11 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
 }
 
 /**
